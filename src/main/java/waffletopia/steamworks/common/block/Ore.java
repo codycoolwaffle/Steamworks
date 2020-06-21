8d19c18b9151bcc64149076efd5935b8fb5ad0b0
ChristopherJTrent <TrentChristopher@student.deanza.edu>
2020-06-21 03:12:09 -0700

    FUCK

--- src/main/java/waffletopia/steamworks/common/block/Ore.java
+++ src/main/java/waffletopia/steamworks/common/block/Ore.java
@@ -9,8 +9,10 @@ import net.minecraftforge.common.ToolType;
 public class Ore
 {
 	public static Block ore_zinc = new Block(
-			Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(1)
+			Block.Properties.create(Material.ROCK)
+							.harvestTool(ToolType.PICKAXE)
+							.harvestLevel(1)
 			                .hardnessAndResistance(3f, 50f))
-			.setRegistryName("steamworks", "ore_zinc");
+							.setRegistryName("steamworks", "ore_zinc");
 	public static BlockItem item_ore_zinc = new BlockItem(ore_zinc, new Item.Properties());
 }
