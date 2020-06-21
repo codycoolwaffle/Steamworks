57ca2e417c00f9a0bc972f8f1aa95cf8240e4e09
codicoolwaffle <codyhaslam@gmail.com>
2020-06-21 01:20:33 -0700

    reformat part 2 electric boogaloo

--- src/main/java/waffletopia/steamworks/common/block/Ore.java
+++ src/main/java/waffletopia/steamworks/common/block/Ore.java
@@ -4,18 +4,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.ToolType;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import javax.annotation.Resource;
-
-public class Ore {
-    public static Block ore_zinc = new Block(Block.Properties.create(Material.ROCK)
-            .harvestTool(ToolType.PICKAXE)
-            .harvestLevel(1)
-            .hardnessAndResistance(3f,50f))
-            .setRegistryName("steamworks","ore_zinc");
-    public static BlockItem item_ore_zinc = new BlockItem(ore_zinc, new Item.Properties());
+public class Ore
+{
+	public static Block ore_zinc = new Block(
+			Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(1)
+			                .hardnessAndResistance(3f, 50f))
+			.setRegistryName("steamworks", "ore_zinc");
+	public static BlockItem item_ore_zinc = new BlockItem(ore_zinc, new Item.Properties());
 }
